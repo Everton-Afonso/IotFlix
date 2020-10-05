8c7a1a58a075a838000a973dddf7812cf711b9cc
Everton Otavio Afonso <evertonafonso0@gmail.com>
2020-10-05 09:52:15 -0300

    Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import dadosIniciais from "./data/dados.json";
+import Banner from "./components/Banner";
 
 function App() {
 	return (
 		<div>
-			<header>Everton</header>
+			<Banner
+				videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+				url={dadosIniciais.categorias[0].videos[0].url}
+				videoDescription={"Projeto IotFlix"}
+			/>
 		</div>
 	);
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { FooterBase } from "./styles";
+
+function Footer() {
+	return (
+		<FooterBase>
+			<a
+				href="https://info.mch.ifsuldeminas.edu.br/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				IFSULDEMINAS
+			</a>
+			<p> Baseado nos conteúdos da imersão React Alura</p>
+		</FooterBase>
+	);
+}
+export default Footer;
