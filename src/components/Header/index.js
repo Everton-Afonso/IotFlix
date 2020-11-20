5055c471af615c98db57dc20e43d7b57a5fde002
Everton Otavio Afonso <evertonafonso0@gmail.com>
2020-11-19 22:20:56 -0300

    Debug vercel

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Logo from "../../assets/logo.png";
-import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom"
 import "./index.css";
 
@@ -10,9 +9,6 @@ function Header() {
 			<a href="/">
 				<img src={Logo} className="logo" alt="IotFlix Logo" />
 			</a>
-			<Button variant="danger">
-				<Link to="/Categorias" className="link">Login</Link> {/* alterar o botão de login quando o matheus pedir*/}
-			</Button>
 		</nav>
 	);
 }
